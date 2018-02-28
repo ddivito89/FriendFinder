b66f730baad4045b5338f5270ca89e28d863d4b6
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-02-27 22:04:22 -0600

    init commit

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+module.exports = function(app) {
+
+app.get("/api/friends", function(req, res) {
+   res.json(false);
+});
+
+app.post("/api/friends", function(req, res) {
+    res.json(false);
+});
+
+}
