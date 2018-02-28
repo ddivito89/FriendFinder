c695acc90e9e201e49383813f63e4ce1f679ee3c
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-02-27 23:32:49 -0600

    wired views,routes, and comparison logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,39 @@
+var friends = require("../data/friends");
+
+
 module.exports = function(app) {
 
 app.get("/api/friends", function(req, res) {
-   res.json(false);
+
+   res.json(friends);
 });
 
 app.post("/api/friends", function(req, res) {
-    res.json(false);
+
+    var newFriend = req.body
+
+    var bestScore = 9999999999
+    var bestMatch = friends[0]
+
+    for (var x=0; x<friends.length; x++){
+
+      var totalDiff = 0;
+
+      for (var y=0; y<friends[x].scores.length; y++){
+        var diff =  Math.abs(friends[x].scores[y] - newFriend.scores[y])
+        totalDiff+=diff
+      }
+
+      if (totalDiff < bestScore){
+        bestScore = totalDiff
+        bestMatch = friends[x]
+      }
+
+    }
+
+    friends.push(newFriend)
+
+    res.json(bestMatch);
 });
 
 }
